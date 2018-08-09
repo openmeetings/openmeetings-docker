ff02271f4f41ec54a2f52a07ba15a63f24c01c49
dhanugithub <37544947+dhanugithub@users.noreply.github.com>
2018-08-09 11:17:28 +0200

    Update om_install.sh

--- scripts/om_install.sh
+++ scripts/om_install.sh
@@ -16,14 +16,14 @@ if [ ! -d "/var/run/mysqld" ]; then
 	mkdir /var/run/mysqld
 fi
 
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
+sudo chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
 
 mysql -u root -p${DB_ROOT_PASS} -se "CREATE DATABASE ${OM_DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci"
 mysql -u root -p${DB_ROOT_PASS} -se "CREATE USER '${OM_DB_USER}'@'localhost' IDENTIFIED BY '${OM_DB_PASS}'"
 mysql -u root -p${DB_ROOT_PASS} -se "grant all privileges on ${OM_DB_NAME}.* to '${OM_DB_USER}'@'localhost'"
 
-chown -R nobody:nogroup ${OM_HOME}
-sudo -u nobody ./admin.sh -v -i -tz Europe/Berlin -email your.email@gmail.com -group group \
+sudo chown -R nobody:nogroup ${OM_HOME}
+sudo -u nobody ./admin.sh -v -i -tz Europe/Berlin -email kulkarnidhana22@gmail.com -group group \
 	-user ${OM_USER} --password ${OM_PASS} \
 	--db-type mysql --db-host localhost --db-name ${OM_DB_NAME} \
 	--db-user ${OM_DB_USER} --db-pass ${OM_DB_PASS}
